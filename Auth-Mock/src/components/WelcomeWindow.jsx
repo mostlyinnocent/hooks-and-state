8e1ed4b7e9891964ed52f2a1bb87225be11acd3f
mostlyinnocent <debdebraj453@gmail.com>
2026-04-18 11:50:21 +0530

    Add welcome window UI

---
+++ Auth-Mock/src/components/WelcomeWindow.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import exitIcon from '../icons/exit.png'
+
+function WelcomeWindow() {
+  return (
+    <div className='h-[100%] w-[70%] bg-gradient-to-t from-orange-400 to-yellow-100 flex flex-col items-start  rounded-xl pt-10 pl-5 gap-2 relative'>
+      <span className='font-extrabold text-4xl font-sans mt-30'>Welcome,</span>
+      <span className='font-bold text-2xl mb-10'>to TenSpark</span>
+      <span>Your work enivironment is ready</span>
+      <p>Everything you need. Nothing you don't.</p>
+      <div className='flex items-center justify-center text-3xl absolute bottom-5 font-bold text-white cursor-pointer gap-0.5'>
+        <span className='transition-transform hover:translate-y-[-2px]'>T</span>
+        <span className='transition-transform hover:translate-y-[-2px]'>e</span>
+        <span className='transition-transform hover:translate-y-[-2px]'>n</span>
+        <span className='transition-transform hover:translate-y-[-2px]'>S</span>
+        <span className='transition-transform hover:translate-y-[-2px]'>p</span>
+        <span className='transition-transform hover:translate-y-[-2px]'>a</span>
+        <span className='transition-transform hover:translate-y-[-2px]'>r</span>
+        <span className='transition-transform hover:translate-y-[-2px]'>k</span>
+        <div className="h-[30px] w-[30px] flex bg-white mask-[url('/stars.png')] mask-no-repeat mask-contain"></div>
+      </div>
+      <div className='h-5 w-5 flex items-center justify-center absolute bottom-5 right-3 cursor-pointer transition-transform hover:scale-105 active:translate-y-1'>
+        <img className='h-full w-full object-cover block' src={exitIcon} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default WelcomeWindow
