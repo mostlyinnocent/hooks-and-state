8449a96e64f0f3ce0e069638023293e8a04eafdb
mostlyinnocent <debdebraj453@gmail.com>
2026-04-25 10:12:01 +0530

    Add backdrop gradient animation and focus input border

--- Multi-Step-Form/src/components/FormWidget.jsx
+++ Multi-Step-Form/src/components/FormWidget.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './FormWidget.css'
 
 
 
 function FormWidget({children}) {
   return (
-      <div className='bg-neutral-950 h-170 w-250 rounded-lg flex items-center pt-4 gap-8 pb-4 pl-4 pr-4 border border-neutral-300/20'>
+      <div className='widget bg-neutral-950 h-170 w-250 rounded-lg flex items-center pt-4 gap-8 pb-4 pl-4 pr-4 border border-neutral-300/20 relative'>
         {children}
       </div>
   )
--- Multi-Step-Form/src/components/TextInputField.jsx
+++ Multi-Step-Form/src/components/TextInputField.jsx
@@ -4,7 +4,7 @@ function TextInputField({label, placeholder, value}) {
   return (
     <div className='flex flex-col h-[80px] w-full gap-2'>
       <span className='text-neutral-200 font-semibold'>{label}</span>
-      <input type="text" value={value} placeholder={placeholder} className='h-10 w-full border border-r-0 border-l-0 border-t-0 outline-0 pl-1.5 border-b-neutral-500 text-neutral-400 focus:border-b-neutral-200'/>
+      <input type="text" value={value} placeholder={placeholder} className='h-10 w-full border border-r-0 border-l-0 border-t-0 outline-0 pl-1.5 border-b-neutral-500 text-neutral-400 focus:border-b-2 focus:border-b-[#068562]'/>
     </div>
   )
 }
